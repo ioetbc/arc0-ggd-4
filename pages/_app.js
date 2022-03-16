cabdb6a269b66fa9af666219bd85cb9e65a7fe83
ioetbc <ioetbc@gmail.com>
2022-03-16 07:06:37 +0000

    adding video

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,12 @@
 import "../styles/globals.css";
 import Head from "next/head";
 
-// import Hammer from "hammerjs";
-// import dynamic from "next/dynamic";
-
-// const Hammer = dynamic(() => import("hammerjs"), { ssr: false });
-
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>My page title</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <script
-          async
-          src="https://hammerjs.github.io/dist/hammer.min.js"
-        ></script>
-        <script
-          async
-          src="https://unpkg.com/propagating-hammerjs@latest/propagating.js"
-        ></script>
       </Head>
       <Component {...pageProps} />
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,50 @@
 import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 import dynamic from "next/dynamic";
+import Script from "next/script";
 import { products } from "../database/products";
 
-const Sketch = dynamic(() => import("react-p5"), { ssr: false });
+const Sketch = dynamic(
+  () =>
+    import("react-p5").then((mod) => {
+      require("p5/lib/addons/p5.sound");
+      return mod.default;
+    }),
+  { ssr: false }
+);
 
 let vx = 0;
 let vy = 0;
 
 let bubbles = [];
-// let unicorn;
 
 let kittens = products;
 
 export default function Home() {
   const router = useRouter();
+  let hammer;
+  let song;
+  let video;
+  let videoSrc = "/videos/snoop.mp4";
 
   useEffect(() => {
     window.addEventListener("wheel", onScroll);
-
-    const hammer = new Hammer(document.body, { preventDefault: true });
-
-    hammer.get("pan").set({
-      direction: Hammer.DIRECTION_ALL,
-    });
-
-    hammer.on("pan", (event) => {
-      for (let b of bubbles) {
-        b.swiped(event);
-      }
-    });
-
     return () => {
       hammer.destroy();
-      window.removeEventListener("whell", onScroll);
+      window.removeEventListener("wheel", onScroll);
     };
   }, []);
 
   const preload = (p5) => {
-    console.log("preload");
     for (let i = 0; i < kittens.length; i++) {
       kittens[i].src = p5.loadImage(kittens[i].src);
     }
-  };
-
-  const onScroll = (event) => {
-    for (let b of bubbles) {
-      b.x += (event.deltaX / 10) * b.parallex;
-      b.y += (event.deltaY / 10) * b.parallex;
-    }
+    p5.soundFormats("mp3");
+    // song = p5.loadSound("/sounds/orgasm-slaps.mp3");
+    p5.noCanvas();
+    video = p5.createVideo(videoSrc);
+    // video.size(600, 900);
+    // video.hide();
   };
 
   const setup = (p5, canvasParentRef) => {
@@ -69,20 +65,14 @@ export default function Home() {
       } = kittens[i];
       let bubble = new Bubble(x, y, width, height, src, parallex, id, p5, url);
       bubbles.push(bubble);
-    }
-    // let unicornKitten = p5.random(kittens);
 
-    // unicorn = new Bubble(
-    //   400,
-    //   200,
-    //   100,
-    //   100,
-    //   unicornKitten.src,
-    //   unicornKitten.parallex,
-    //   unicornKitten.id,
-    //   p5,
-    //   unicornKitten.url
-    // );
+      video.volume(0);
+      video.loop();
+      video.hide();
+
+      let videoBubble = new Bubble(1200, 300, 200, 300, video, 0, 2, p5, false);
+      bubbles.push(videoBubble);
+    }
 
     canvas.mousePressed(() => {
       for (let b of bubbles) {
@@ -91,16 +81,23 @@ export default function Home() {
     });
   };
 
+  const soundIsPlaying = false;
+
   const draw = (p5) => {
     p5.background(0);
-    // unicorn.x = p5.mouseX;
-    // unicorn.y = p5.mouseY;
-    // unicorn.show();
-    // unicorn.move();
     for (let b of bubbles) {
       b.show();
-      // vx = p5.constrain(vx, 0, 3357);
-      // vy = p5.constrain(vy, 0, 2518);
+      if (b.mouseOverProduct(p5.mouseX, p5.mouseY) && !soundIsPlaying) {
+        // song.play();
+        soundIsPlaying = true;
+      }
+    }
+  };
+
+  const onScroll = (event) => {
+    for (let b of bubbles) {
+      b.x += (event.deltaX / 10) * b.parallex;
+      b.y += (event.deltaY / 10) * b.parallex;
     }
   };
 
@@ -119,14 +116,22 @@ export default function Home() {
     }
 
     move() {
-      // this.x = this.x + this.p5.random(-5, 5);
-      // this.y = this.y + this.p5.random(-5, 5);``
+      this.x = this.x + this.p5.random(-5, 5);
+      this.y = this.y + this.p5.random(-5, 5);
+    }
+
+    isProduct() {
+      return this.url;
     }
 
     show() {
       this.p5.image(this.kitten, this.x, this.y, this.width, this.height);
       this.x += vx;
       this.y += vy;
+
+      if (this.url) {
+        this.move();
+      }
 
       vx *= 0.95;
       vy *= 0.95;
@@ -146,6 +151,16 @@ export default function Home() {
       return distance < this.width + otherBubble.width;
     }
 
+    mouseOverProduct(mouseX, mouseY) {
+      return (
+        mouseX > this.x &&
+        mouseX < this.x + this.width &&
+        mouseY > this.y &&
+        mouseY < this.y + this.height &&
+        this.url
+      );
+    }
+
     clicked(mouseX, mouseY) {
       if (
         mouseX > this.x &&
@@ -159,14 +174,31 @@ export default function Home() {
     }
 
     swiped = (event) => {
-      console.log("event.velocityY", event.velocityY * kittens.length);
-      console.log("kittens.length", kittens.length);
       vx += event.velocityX * this.parallex;
-      // vx += event.velocityX * 10;
       vy += event.velocityY * this.parallex;
-      // vy += event.velocityY * 10;
     };
   }
 
-  return <Sketch setup={setup} draw={draw} preload={preload} />;
+  return (
+    <>
+      <Script
+        id="hammer"
+        src="https://hammerjs.github.io/dist/hammer.min.js"
+        onLoad={() => {
+          hammer = new Hammer(document.body, { preventDefault: true });
+
+          hammer.get("pan").set({
+            direction: Hammer.DIRECTION_ALL,
+          });
+
+          hammer.on("pan", (event) => {
+            for (let b of bubbles) {
+              b.swiped(event);
+            }
+          });
+        }}
+      ></Script>
+      <Sketch setup={setup} draw={draw} preload={preload} />
+    </>
+  );
 }
