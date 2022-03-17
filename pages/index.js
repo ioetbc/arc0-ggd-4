e1b168e530011aa6cd859a92903024ef721462b5
ioetbc <ioetbc@gmail.com>
2022-03-17 20:09:40 +0000

    refactoring

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,11 @@ import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 import dynamic from "next/dynamic";
 import Script from "next/script";
+
 import { onScroll } from "../utils/onScroll";
 import { products } from "../database/products";
-import Bubble from "../utils/Bubble";
-
-import { preloadCanvas } from "../utils/preloadCanvas";
-import { SetupCanvas } from "../utils/setupCanvas";
-import { drawCanvas } from "../utils/drawCanvas";
+import { handleBubbleClick } from "../utils/handleBubbleClick";
+import { createBubbles } from "../utils/createBubbles";
 
 const Sketch = dynamic(
   () =>
@@ -22,16 +20,12 @@ const Sketch = dynamic(
 export default function Home() {
   const router = useRouter();
   let bubbles = [];
-
   let hammer;
-  let song;
   let video;
   let logo;
   let canvas;
   let introVideo;
   let showIntroVideo = true;
-  let hasIntroVideoLoaded = false;
-  let showSkipButton = false;
   let skipButton;
   let mediaLoaded = 0;
 
@@ -47,65 +41,28 @@ export default function Home() {
 
   const preload = (p5) => {
     introVideo = p5.createVideo("/videos/intro-video.mp4", () =>
-      introVideoLoaded(p5)
+      handleSecondaryMediaLoading(p5)
     );
   };
 
-  const introVideoLoaded = (p5) => {
+  const handleSecondaryMediaLoading = (p5) => {
     introVideo.volume(0);
     introVideo.loop();
     introVideo.hide();
-    // hasIntroVideoLoaded = true;
+    ({ video, bubbles } = createBubbles(
+      p5,
+      products,
+      bubbles,
+      video,
+      handleMediaLoaded
+    ));
 
-    for (let p of products) {
-      if (p.type === "video") {
-        video = p5.createVideo(p.src, () => mediaLoadedFunc(p5));
-      } else {
-        p.src = p5.loadImage(p.src, () => mediaLoadedFunc(p5));
-      }
-    }
     logo = p5.loadImage("/images/webp/logo.webp");
-    p5.soundFormats("mp3");
-    song = p5.loadSound("/sounds/orgasm-slaps.mp3");
-
-    for (let product of products) {
-      const {
-        x,
-        y,
-        width,
-        height,
-        src,
-        parallex,
-        id,
-        url = false,
-        type,
-      } = product;
-
-      const dynamicSrc = type === "video" ? video : src;
-
-      let bubble = new Bubble(
-        x,
-        y,
-        width,
-        height,
-        dynamicSrc,
-        parallex,
-        id,
-        p5,
-        url,
-        type
-      );
-
-      bubbles.push(bubble);
-    }
   };
 
-  // SETUP IS ONLY CALLED ONCE ALL THE MEDIA HAS LOADED
-  const mediaLoadedFunc = (p5) => {
+  const handleMediaLoaded = (p5) => {
     mediaLoaded++;
-    console.log("mediaLoaded", mediaLoaded);
     if (mediaLoaded === products.length) {
-      console.log("show the skip button");
       skipButton = p5.createButton("skip intro video");
       skipButton.position(0, 0);
       skipButton.style("background-color", "#ff0000");
@@ -118,25 +75,14 @@ export default function Home() {
       .createCanvas(window.innerWidth, window.innerHeight)
       .parent(canvasParentRef);
 
-    let isPlayingVideoSound = false;
     canvas.mousePressed(() => {
-      for (let b of bubbles) {
-        const bubbleClicked = b.clicked(p5.mouseX, p5.mouseY);
-        if (bubbleClicked) {
-          if (b.url) {
-            window.location.pathname = b.url;
-          }
-          if (b.type === "video") {
-            video.volume(isPlayingVideoSound ? 0 : 1);
-            isPlayingVideoSound = !isPlayingVideoSound;
-          }
-        }
-      }
+      handleBubbleClick(p5, bubbles, video);
     });
   };
 
   const draw = (p5) => {
     p5.background(255);
+    video.hide(); // NEED TO FIX THE VIDEO
 
     if (showIntroVideo) {
       p5.image(introVideo, 0, 0, p5.width, p5.height);
@@ -145,26 +91,10 @@ export default function Home() {
         if (b.type === "video") {
           video.volume(0);
           video.loop();
-          video.hide();
         }
         b.show();
-        // if (b.mouseOver(p5.mouseX, p5.mouseY)) {
-        //   document.querySelector("canvas").style.cursor = "pointer";
-        // } else {
-        //   document.querySelector("canvas").style.cursor = "default";
-        // }
       }
     }
-
-    // TODO the click event is not working correctly (its really low down) becuase of the logo
-    // const logoWidth = 75;
-    // const logoHeight = 75;
-    // const logoOffsetX = 24;
-    // const logoOffsetY = 24;
-    // p5.translate(p5.width - logoWidth + logoOffsetX, logoHeight - logoOffsetY);
-    // p5.rotate(p5.mouseY / 200);
-    // p5.imageMode(p5.CENTER);
-    // p5.image(logo, 0, 0, logoWidth, logoHeight);
   };
 
   return (
---
+++ utils/createBubbles.js
@@ -0,0 +1,35 @@
+import Bubble from "../utils/Bubble";
+
+export const createBubbles = (
+  p5,
+  products,
+  bubbles,
+  video,
+  handleMediaLoaded
+) => {
+  for (let p of products) {
+    if (p.type === "video") {
+      video = p5.createVideo(p.src, () => handleMediaLoaded(p5));
+    } else {
+      p.src = p5.loadImage(p.src, () => handleMediaLoaded(p5));
+      let bubble = new Bubble(
+        p?.x,
+        p?.y,
+        p?.width,
+        p?.height,
+        p?.type === "video" ? video : p?.src,
+        p?.parallex,
+        p?.id,
+        p5,
+        p?.url,
+        p?.type
+      );
+      bubbles.push(bubble);
+    }
+  }
+
+  return {
+    video,
+    bubbles,
+  };
+};
