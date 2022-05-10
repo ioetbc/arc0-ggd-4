b3196647d573394fb4fbd82a16eec367da6800b1
ioetbc <ioetbc@gmail.com>
2022-05-10 17:34:47 +0100

    use webp

--- pages/landing-page.jsx
+++ pages/landing-page.jsx
@@ -2,47 +2,69 @@ import React, { useEffect, useState } from "react";
 import Router from "next/router";
 import styled from "styled-components";
 
+const Container = styled.div`
+  /* padding: 16px; */
+  box-sizing: border-box;
+`;
+
 const BgImage = styled.div`
   width: 100%;
   height: 100vh;
   background-image: url("/images/landing-page/landing-bg.webp");
   background-position: center;
-  transition: filter 0.5s ease;
+  transition: all 0.5s ease;
   filter: ${(props) => (props.removeFilter ? "invert(0)" : "invert(100%)")};
+  opacity: ${(props) => (props.hideBg ? 0 : 1)};
 `;
 
 const Door = styled.div`
   .door {
     position: absolute;
     width: 16vh;
-    left: 50%;
-    transform: translateX(-50%);
     top: 56%;
     height: 32vh;
-    opacity: 0.6;
+    left: 50%;
+    transform: translateX(-50%);
     cursor: cell;
+    filter: invert(100%);
+    transition: filter 0.5s ease;
+    &:hover {
+      filter: ${(props) => (props.hideBg ? "invert(100%)" : "invert(0)")};
+    }
+  }
+
+  .hoverDoor {
   }
 `;
 
 const LandingPage = () => {
   const [removeFilter, setRemoveFilter] = useState(false);
+  const [hideBg, setHideBG] = useState(false);
   const handleDoorOpen = () => {
-    Router.push("landing-page-video");
+    setHideBG(true);
+    setTimeout(() => {
+      Router.push("landing-page-video");
+    }, 2000);
   };
 
   useEffect(() => {
     setTimeout(() => {
       setRemoveFilter(true);
-    }, 1000);
+    }, 100);
   }, []);
 
   return (
-    <>
-      <BgImage removeFilter={removeFilter}></BgImage>
-      <Door>
+    <Container>
+      <BgImage removeFilter={removeFilter} hideBg={hideBg}></BgImage>
+      <Door hideBg={hideBg}>
+        {/* <img
+          src="/images/landing-page/door-hover.png"
+          className="test-image"
+          alt="door"
+        ></img> */}
         <div className="door" onClick={handleDoorOpen}></div>
       </Door>
-    </>
+    </Container>
   );
 };
 
