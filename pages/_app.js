66c1ebdcede34d2da02790a5c4493008682f3d1a
ioetbc <ioetbc@gmail.com>
2022-05-10 16:12:28 +0100

    add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>My page title</title>
+        <title>ARC-GGD</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
