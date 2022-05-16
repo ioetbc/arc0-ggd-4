e2098d10a3d7fb94271ebcf8592073516553bf41
ioetbc <ioetbc@gmail.com>
2022-05-16 13:49:14 +0100

    preload video

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,11 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>ARC-GGD</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          rel="preload"
+          href="/videos/intro-video.mp4"
+          as="introVideo"
+        ></link>
       </Head>
       <Component {...pageProps} />
     </>
