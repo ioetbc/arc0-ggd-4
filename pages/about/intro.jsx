6fc9afed35bf52e5d0d9be9a4f2b0cc202741287
ioetbc <ioetbc@gmail.com>
2022-06-24 18:10:11 +0100

    stuff

--- pages/about/intro.jsx
+++ pages/about/intro.jsx
@@ -1,53 +1,53 @@
-import React from "react";
-import styled from "styled-components";
+// import React from "react";
+// import styled from "styled-components";
 
-const Container = styled.div`
-  background: blue;
-  color: yellow;
-  padding: 1.8em 2.4em;
-  display: flex;
-  flex-direction: column;
-  row-gap: 0.6em;
-`;
+// const Container = styled.div`
+//   background: blue;
+//   color: yellow;
+//   padding: 1.8em 2.4em;
+//   display: flex;
+//   flex-direction: column;
+//   row-gap: 0.6em;
+// `;
 
-const Pre = styled.div`
-  display: flex;
-  justify-content: space-between;
+// const Pre = styled.div`
+//   display: flex;
+//   justify-content: space-between;
 
-  p {
-    font-size: 1.2em;
-  }
-`;
+//   p {
+//     font-size: 1.2em;
+//   }
+// `;
 
-const Heading = styled.h1`
-  font-size: 2.2em;
-  text-transform: uppercase;
-`;
+// const Heading = styled.h1`
+//   font-size: 2.2em;
+//   text-transform: uppercase;
+// `;
 
-const Info = styled.div`
-  display: flex;
-  justify-content: space-between;
-  gap: 48px;
-  font-size: 1.6em;
-`;
+// const Info = styled.div`
+//   display: flex;
+//   justify-content: space-between;
+//   gap: 48px;
+//   font-size: 1.6em;
+// `;
 
-const Intro = () => {
-  return (
-    <Container>
-      <Pre>
-        <p>Arc-GGD building Services Provision &amp; Estate Management Ltd.</p>
-        <p>
-          Arc-GGD Gesellschaft fur Gebaudedienstleistung und
-          immobilienverwaltunf GmbH.
-        </p>
-      </Pre>
-      <Heading>Kompetent . preiswert . flexibel</Heading>
-      <Info>
-        <h2>general information</h2>
-        <h2>allgemeine informationen</h2>
-      </Info>
-    </Container>
-  );
-};
+// const Intro = () => {
+//   return (
+//     <Container>
+//       <Pre>
+//         <p>Arc-GGD building Services Provision &amp; Estate Management Ltd.</p>
+//         <p>
+//           Arc-GGD Gesellschaft fur Gebaudedienstleistung und
+//           immobilienverwaltunf GmbH.
+//         </p>
+//       </Pre>
+//       <Heading>Kompetent . preiswert . flexibel</Heading>
+//       <Info>
+//         <h2>general information</h2>
+//         <h2>allgemeine informationen</h2>
+//       </Info>
+//     </Container>
+//   );
+// };
 
-export default Intro;
+// export default Intro;
